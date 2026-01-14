a250d28372a5f8513f5963f66be7123c27fbfd0a
dagunliyu <lhcxndx@126.com>
2026-01-15 00:18:19 +0800

    remove unnecessary files

--- gltest/Chapter_12_tessellation/Prog12_1_tessellationGridOnly/vertShader.glsl
+++
@@ -1,7 +0,0 @@
-#version 430
-
-uniform mat4 mvp_matrix;
-
-void main(void)
-{
-}
--- gltest/GLSLReader.cpp
+++
@@ -1,6 +0,0 @@
-#include "GLSLReader.h"
-
-std::string readShaderSource(const char *filePath)
-{
-    return std::string();
-}
