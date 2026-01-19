c6b4c054c431cd86eb02ce042b965954e84fd6bc
dagunliyu <lhcxndx@126.com>
2026-01-20 00:14:40 +0800

    update

--- cMake/FindCoin.cmake
+++ cMake/FindCoin.cmake
@@ -8,21 +8,25 @@
 find_path(COIN_INCLUDE_DIR 
     NAMES Inventor/So.h
     PATHS
+        ${CMAKE_SOURCE_DIR}/external/coin/include
         /usr/include
         /usr/local/include
         /opt/local/include
         $ENV{COINDIR}/include
+        "C:/Program Files/Coin3D/include"
     PATH_SUFFIXES coin4 coin3 coin2
 )
 
 find_library(COIN_LIBRARY
-    NAMES Coin coin4 coin3 coin2
+    NAMES Coin Coin4 coin4 coin3 coin2
     PATHS
+        ${CMAKE_BINARY_DIR}/external/coin
         /usr/lib
         /usr/local/lib
         /opt/local/lib
         $ENV{COINDIR}/lib
-    PATH_SUFFIXES x86_64-linux-gnu
+        "C:/Program Files/Coin3D/lib"
+    PATH_SUFFIXES x86_64-linux-gnu Release Debug
 )
 
 # Handle the QUIETLY and REQUIRED arguments
