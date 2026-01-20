bc0f56a3f950b27113b777bc6b89db7ee0bf0bfe
dgliyu <lhcxndx@126.com>
2026-01-20 09:51:51 +0800

    add soqt

--- cMake/FindCoin.cmake
+++ cMake/FindCoin.cmake
@@ -5,6 +5,20 @@
 #  COIN_LIBRARY - The Coin3D library
 #  COIN_VERSION - The version of Coin3D found
 
+if(TARGET Coin)
+    set(COIN_FOUND TRUE)
+    set(COIN_LIBRARY Coin)
+    set(COIN_LIBRARIES Coin)
+    get_target_property(COIN_SOURCE_DIR Coin SOURCE_DIR)
+    get_target_property(COIN_BINARY_DIR Coin BINARY_DIR)
+    set(COIN_INCLUDE_DIR "${COIN_SOURCE_DIR}/include" "${COIN_BINARY_DIR}/include")
+    set(COIN_INCLUDE_DIRS ${COIN_INCLUDE_DIR})
+    if(NOT TARGET Coin::Coin)
+        add_library(Coin::Coin ALIAS Coin)
+    endif()
+    return()
+endif()
+
 find_path(COIN_INCLUDE_DIR 
     NAMES Inventor/So.h
     PATHS
